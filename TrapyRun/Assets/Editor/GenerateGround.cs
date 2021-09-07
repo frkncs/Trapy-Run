576a8b4e1504bae7f53350133ef51e783709cca2
frkncs <hesapcocnd@gmail.com>
2021-09-07 17:51:35 +0300

    Added Generate Ground button to inspector. Now we can generate ground when game is not running!

---
+++ TrapyRun/Assets/Editor/GenerateGround.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GroundGenerator))]
+public class GenerateGround : Editor
+{
+    GameObject cube;
+
+    public override void OnInspectorGUI()
+    {
+        initializeVariables();
+        
+        base.OnInspectorGUI();
+
+        GroundGenerator gg = (GroundGenerator)target;
+
+        GUILayout.Space(15);
+
+        if (GUILayout.Button("Generate Ground"))
+        {
+            gg.generateGround(cube, 5, 150);
+        }
+    }
+
+    void initializeVariables()
+    {
+        cube = Resources.Load<GameObject>("Prefabs/Cube");
+    }
+}
--- TrapyRun/Assets/Scripts/GroundGenerator.cs
+++ TrapyRun/Assets/Scripts/GroundGenerator.cs
@@ -9,20 +9,12 @@ public class GroundGenerator : MonoBehaviour
     // Public Variables
 
     // Private Variables
-    GameObject cube;
 
     float groundStartZPos = -10;
 	
 	#endregion
 
-    void Start()
-    {
-        cube = Resources.Load<GameObject>("Prefabs/Cube");
-
-        generateGround(5, 150);
-    }
-
-    void generateGround(int row, int column)
+    public void generateGround(GameObject cube, int row, int column)
     {
         // Row count for just one side
         // eg. [row] left + [row] right + 1 center
