ea3ae8145128772d0e60c76b9d2431dc014d4964
frkncs <hesapcocnd@gmail.com>
2021-09-07 14:45:53 +0300

    Updated scene and adding player controller script to player

---
+++ TrapyRun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+
+    // Public Variables
+
+    // Private Variables
+    float speed = 10;
+	
+	#endregion
+
+    void Update()
+    {
+        MoveForward();
+    }
+
+    void MoveForward()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
