751b692c64e91d67109c0efd2aacdddf02cca6ec
frkncs <hesapcocnd@gmail.com>
2021-09-07 14:55:43 +0300

    Added ability to move player horizontally

--- TrapyRun/Assets/Scripts/PlayerController.cs
+++ TrapyRun/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     // Public Variables
 
     // Private Variables
+    float firstFingerX, lastFingerX;
+
     float speed = 10;
 	
 	#endregion
@@ -16,10 +18,36 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MoveForward();
+        MoveHorizontal();
     }
 
     void MoveForward()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void MoveHorizontal()
+    {
+        float mousePosX = getMousePos();
+
+        if (Input.GetMouseButtonDown(0)) firstFingerX = mousePosX;
+        else if (Input.GetMouseButton(0))
+        {
+            lastFingerX = mousePosX;
+
+            float dif = lastFingerX - firstFingerX;
+
+            transform.position += new Vector3(dif, 0, 0);
+
+            firstFingerX = lastFingerX;
+        }
+    }
+
+    float getMousePos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = transform.position.z;
+
+        return Camera.main.ScreenToWorldPoint(mousePos).x;
     }
 }
